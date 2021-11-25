75ace00502cd72c24aae51940a81cff101364d2f
ivan valle <54561077+Ivanvallesj@users.noreply.github.com>
2021-11-25 12:47:41 -0300

    ControlDeExceptions1.0.1.0

--- CosultorioDescktop/Program.cs
+++ CosultorioDescktop/Program.cs
@@ -2,6 +2,7 @@ using ConsultorioDesktop.Forms;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -19,6 +20,22 @@ namespace ConsultorioDesktop
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FrmInicio());
+        }
+        private static void DespliegueControladoDeErroresOtroshilos(object sender, UnhandledExceptionEventArgs args)
+        {
+            Exception exepcion = (Exception)args.ExceptionObject;
+
+            // MessageBox.Show($"Ha ocurrido un error:{e.Message}{System.Environment.NewLine}Origen del error:{e.Source}");
+            var frmError = new FrmError(exepcion);
+            frmError.ShowDialog();
+        }
+
+        private static void DespliegueControladoDeErroresHiloPrincipal(object sender, ThreadExceptionEventArgs e)
+        {
+
+            //MessageBox.Show($"Ha ocurrido un error:{e.Exception.Message}{System.Environment.NewLine}Origen del error:{e.Exception.Source}");
+            var frmError = new FrmError(e.Exception);
+            frmError.ShowDialog();
         }
     }
 }
